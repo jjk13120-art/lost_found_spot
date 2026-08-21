c40e3a20b44f20bf69e93feb15ac890ae7aae947
jjk13120-art <jjk13120@gmail.com>
2026-08-21 11:33:38 +0530

    Remove hardcoded sensitive email credentials from settings.py

--- mylostfound/settings.py
+++ mylostfound/settings.py
@@ -153,8 +153,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'jjk13120@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'kjilknwobtcfltqe')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'your-email@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'your-app-password')
 
 # WhiteNoise storage settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
